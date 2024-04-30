1a14185eed46e6960efe9cb5e2f93b6741e120de
Tri Nguyen <tringuyen9295@gmail.com>
2024-04-30 20:49:49 +1000

    add another route

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const express = require("express");
 const path = require("path");
 
 const PORT = process.env.PORT || 3001;
-
 const app = express();
 
 app.use(express.static("public"));
@@ -13,6 +12,12 @@ app.use(express.static("public"));
 app.get("/", (req, res) =>
 	res.sendFile(path.join(__dirname, "/public/index.html"))
 );
+
+app.get("/another-route", (req, res) => {
+	res.json({
+		hello: "world",
+	});
+});
 
 app.get("/secret", (req, res) => {
 	console.log("credential", process.env.USER);
