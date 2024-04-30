ae3c59e5d9e60dfc444cacd51c8267b50b5b4de1
Tri Nguyen <tringuyen9295@gmail.com>
2024-04-30 20:28:53 +1000

    initial files

---
+++ server.js
@@ -0,0 +1,22 @@
+require("dotenv").config();
+
+const express = require("express");
+const path = require("path");
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.use(express.static("public"));
+
+// GET Route for homepage
+app.get("/", (req, res) =>
+	res.sendFile(path.join(__dirname, "/public/index.html"))
+);
+
+app.get("/secret", (req, res) => {
+	console.log("credential", process.env.USER);
+	res.end();
+});
+
+app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
